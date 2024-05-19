751ba64afb8cc5ebc9d660a37647406d3b143f31
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-05-19 16:55:54 +0700

    auth.module:import TypeOrmModule & provides SessionSerializer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './utils/GoogleStrategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { SessionSerializer } from './utils/Serializer';
+
 
 @Module({
+  imports:[TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [
+    AuthService, 
+    GoogleStrategy, 
+    SessionSerializer,
+    {
+      provide:"AUTH_SERVICE",
+      useClass:AuthService
+   }
+],
 })
 export class AuthModule {}
